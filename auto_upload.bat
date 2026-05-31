a9abf440e687b8121f6dfd15922983cc1009269c
momentshk <vsc.billy@gmail.com>
2026-05-31 19:31:06 +0800

    Auto sync: 31/05/2026 週日 19:31:06.38

--- auto_upload.bat
+++ auto_upload.bat
@@ -2,13 +2,17 @@
 :: 切換到檔案夾所在位置
 cd /d "%~dp0"
 
-:: 1. 自動將新複製進來的巴士相、檔案加入暫存區
+:: 1. 執行 Fetch & Pull：先檢查並拉低雲端最新變更，確保兩邊同步
+git fetch origin Bus-Display
+git pull origin Bus-Display
+
+:: 2. 自動將你新複製進來的檔案加入暫存區
 git add .
 
-:: 2. 免寫 Summary：自動用當時嘅日期時間作為 Summary 並 Commit
+:: 3. 自動用當時嘅日期時間作為 Summary 並 Commit
 git commit -m "Auto sync: %date% %time%"
 
-:: 3. 免撳 Push：全自動推上雲端 (跟返你第一張圖嘅 Current branch 名稱)
+:: 4. 全自動推上雲端
 git push origin Bus-Display
 
 exit
